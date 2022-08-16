f8ef4e815adcefb02f76218fb46a4a64bb377b5e
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2022-08-16 17:46:48 +0100

    Added feature to create new to-do items

--- test_main.py
+++ test_main.py
@@ -9,3 +9,25 @@ def test_root_returns_200_status_and_json_response():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_create_todo_item_returns_200_status_and_json_response():
+    response = client.post(
+        "/todos", json={"id": 1234, "title": "Task One", "complete": False}
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1234,
+        "title": "Task One",
+        "complete": False,
+    }
+
+
+def test_create_existing_todo_returns_400_status_and_error():
+    response = client.post(
+        "/todos", json={"id": 1234, "title": "Task One", "complete": False}
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "To-do item with id 1234 already exists",
+    }
